a5eca2e30be3b7e0a6483c3a5a962cf339187690
CAN1177 <yican1175@163.com>
2023-05-15 14:49:29 +0800

    feat: 新增lint-staged优化eslint检测

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react/jsx-runtime'
   ],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn'
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect'
+    }
   }
 }
