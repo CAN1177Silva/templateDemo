1752a72f37ada176c52fd8107a74e41fcc08d765
yifosheng <yican1175@163.com>
2023-05-15 11:16:59 +0800

    feat: 新增自动格式化配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,14 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true,
-    "node": true
-  },
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    'plugin:react/jsx-runtime'
+  env: { browser: true, es2020: true, node: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  "overrides": [
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
-  },
-  "plugins": [
-      "react",
-      "@typescript-eslint"
-  ],
-  "rules": {
-    '@typescript-eslint/no-non-null-assertion': 'off'
-  },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn'
   }
 }
--- src/README.md
+++
@@ -1,2 +0,0 @@
-# templateDemo
-react+ts+vite的后台管理模板，包含代码规范
