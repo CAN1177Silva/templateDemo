6936dae520d9216a7c8f3dbdd51d4be19031afd3
Silva <39960526+CAN1177@users.noreply.github.com>
2023-05-18 23:06:28 +0800

    feat:尝试zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  function hello(name: string) {
-    console.log('%c Line:10 🍒', 'color:#93c0a4', name)
-  }
-  hello('manJi')
+  const [count,  setCount] = useState(0)
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel='noreferrer'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
         <p>
